54b8d244f90a153e5c7530eec68d0f2cdef29f7c
Mario <mculajayr@miumg.edu.gt>
2025-10-24 12:14:36 -0600

    eliminar archivos .js

--- Cliente/src/app/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-export default function AppShell({ children }) {
-    const { user, logout } = useAuth();
-    const nav = useNavigate();
-    return (_jsxs(_Fragment, { children: [_jsx(Navbar, { bg: "dark", variant: "dark", expand: "sm", children: _jsxs(Container, { children: [_jsx(Navbar.Brand, { as: Link, to: "/", children: "Votaciones" }), _jsxs(Nav, { className: "ms-auto", children: [!user && _jsx(Nav.Link, { as: Link, to: "/login", children: "Login" }), !user && _jsx(Nav.Link, { as: Link, to: "/register", children: "Register" }), user && _jsxs(Navbar.Text, { className: "me-3", children: ["Hola, ", user.nombre] }), user && _jsx(Nav.Link, { onClick: () => { logout(); nav('/login'); }, children: "Salir" })] })] }) }), _jsx(Container, { className: "py-4", children: children })] }));
-}
--- Cliente/src/modules/admin/SidebarMenu.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-// src/modules/admin/SidebarMenu.tsx
-import { Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-export default function SidebarMenu() {
-    return (_jsxs(Nav, { className: "flex-column bg-light shadow-sm p-3 rounded", children: [_jsx("h5", { className: "fw-bold mb-3 text-primary", children: "Panel de Administraci\u00F3n" }), _jsx(NavLink, { to: "/admin", end: true, className: ({ isActive }) => `nav-link ${isActive ? 'fw-bold text-primary bg-white rounded px-2' : ''}`, children: "\uD83D\uDDF3\uFE0F Administrar Campa\u00F1as" }), _jsx(NavLink, { to: "/admin/campaigns/new", className: ({ isActive }) => `nav-link ${isActive ? 'fw-bold text-primary bg-white rounded px-2' : ''}`, children: "\u2795 Crear Campa\u00F1a" }), _jsx(NavLink, { to: "/admin/reports", className: ({ isActive }) => `nav-link ${isActive ? 'fw-bold text-primary bg-white rounded px-2' : ''}`, children: "\uD83D\uDCCA Reportes" })] }));
-}
